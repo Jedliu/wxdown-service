3f139ec17397575c2d10ccdee9b8abbfa2ae959b
jock <findsource@proton.me>
2025-06-09 10:41:28 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import mitm
-import asyncio
 import watcher
 import utils
 
@@ -12,19 +11,15 @@ def main():
         print("启动 mitmproxy 失败，请切换端口进行重试")
         sys.exit(1)
 
+    print(f"mitmproxy 代理地址: {proxy_address}")
+
     # 检查证书是否安装，以及代理设置是否正确
-    utils.wait_until_env_configured()
+    utils.wait_until_env_configured(proxy_address)
 
-    watcher.start(proxy_address)
-
+    # 启动文件监控及 ws 服务
+    watcher.start()
 
 
 if __name__ == '__main__':
-    # print(utils.proxy_correct())
-    # print(utils.is_certificate_installed_macos())
-    # proxy = urllib.request.getproxies()
-    # print(proxy)
-    # response = requests.get('http://mitm.it', proxies=proxy)
-    # print(response.text)
     main()
 
--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 import re
 import subprocess
-import time
 import urllib.request
 import requests
 
-def proxy_correct():
+def is_proxy_correct(target_proxy_address):
     proxy = urllib.request.getproxies()
-    print(proxy)
     response = requests.get('http://mitm.it', proxies=proxy).text
-    if re.search(r'If you can see this, traffic is not passing through mitmproxy', response):
+
+    if re.search(r'If you can see this, traffic is not passing through mitmproxy', response) or proxy != target_proxy_address:
+        print("\n检测到系统的网络代理设置不正确")
+        print(f"当前网络代理: {proxy}")
+        print(f"目标网络代理: {target_proxy_address}")
         return False
     else:
         return True
+
 
 cert_name = 'mitmproxy'
 
@@ -50,6 +53,7 @@ def is_certificate_installed_windows():
                                 return True
         return False
 
+# 检测证书是否安装
 def wait_until_certificate_installed():
     while True:
         if is_certificate_installed_macos():
@@ -57,8 +61,17 @@ def wait_until_certificate_installed():
         else:
             input("系统中未检测到 mitmproxy 的证书，请进行手动安装。\n证书安装教程请参考: https://docs.mitmproxy.org/stable/concepts/certificates/#installing-the-mitmproxy-ca-certificate-manually\n\n证书安装后请按任意键继续")
 
-def wait_until_env_configured():
+# 检查系统代理是否设置正确
+def wait_until_proxy_setting(proxy_address):
+    while True:
+        if is_proxy_correct({"http": proxy_address, "https": proxy_address}):
+            break
+        else:
+            input("\n按任意键继续")
+
+def wait_until_env_configured(proxy_address = None):
     # 检查证书是否安装
     wait_until_certificate_installed()
 
     # 检查系统代理是否设置正确
+    wait_until_proxy_setting(proxy_address)
