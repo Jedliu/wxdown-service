6cbe12b9b0eab6f1c371e0cbb10591e8d325b804
jock <findsource@proton.me>
2025-06-11 23:49:57 +0800

    update

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import multiprocessing
 import sys
+
 import mitm
-import watcher
 import utils
+import watcher
 
 
 def main():
     # 启动 mitmproxy 进程
-    proxy_address = mitm.start()
-    if proxy_address is None:
+    mitm_proxy_address = mitm.start()
+    if mitm_proxy_address is None:
         utils.print_error_message("启动 mitmproxy 失败，请切换端口进行重试")
         sys.exit(1)
 
-    # utils.print_info_message(f"mitmproxy listening at {proxy_address}")
+    # utils.print_info_message(f"mitmproxy listening at {mitm_proxy_address}")
 
     # 检查环境是否配置正确
-    utils.wait_until_env_configured(proxy_address)
+    utils.wait_until_env_configured(mitm_proxy_address)
 
     # 启动文件监控及 ws 服务
     watcher.start()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+import time
 import urllib.request
 import requests
 import cert
@@ -6,29 +7,41 @@ import version
 
 from logger import logger
 from termcolor import colored
+from yaspin import yaspin
 
 
 # 检查代理是否正确
-def is_proxy_setting(target_proxy_address):
-    proxy = urllib.request.getproxies()
-    logger.debug(f"检测到系统代理设置为: {proxy}")
-    logger.debug(f"mitmproxy 代理为: {target_proxy_address}")
+def is_proxy_setting(mitm_proxy_obj):
+    with yaspin(text="检查中"):
+        proxy_obj = urllib.request.getproxies()
+        logger.debug(f"检测到系统代理设置为: {proxy_obj}")
+        logger.debug(f"mitmproxy 代理为: {mitm_proxy_obj}")
 
-    try:
-        response = requests.get('http://mitm.it', proxies=proxy).text
-    except requests.exceptions.ProxyError as e:
-        print_error_message("\n代理配置有误，请检查设置")
-        logger.error(f"检测 http://mitm.it 代理时出错: {e}")
-        return False
+        try:
+            response = requests.get('http://mitm.it', proxies=proxy_obj).text
+        except requests.exceptions.ProxyError as e:
+            print_error_message("\n代理配置有误，请检查设置")
+            logger.error(f"检测 http://mitm.it 代理时出错: {e}")
+            return False
 
-    is_match = re.search(r'If you can see this, traffic is not passing through mitmproxy', response)
-    if is_match or proxy['http'] != target_proxy_address['http'] or proxy['https'] != target_proxy_address['https']:
-        print_error_message("\n检测到系统的网络代理设置不正确")
-        print(f"当前网络代理: {proxy}")
-        print(f"目标网络代理: {target_proxy_address}")
-        return False
+    traffic_not_passing = re.search(r'If you can see this, traffic is not passing through mitmproxy', response)
+    if traffic_not_passing:
+        # 流量未经过 mitmproxy
+        success = False
+    elif proxy_obj.keys() < {'http', 'https'}:
+        # 代理需要全部设置
+        success = False
+    elif proxy_obj['http'] != mitm_proxy_obj['http'] or proxy_obj['https'] != mitm_proxy_obj['https']:
+        success = False
     else:
-        return True
+        success = True
+
+    if not success:
+        print_error_message("\n检测到系统的网络代理设置不正确")
+        print(f"当前网络代理: {proxy_obj}")
+        print(f"目标网络代理: {mitm_proxy_obj}")
+
+    return success
 
 
 # 检查证书是否安装
@@ -44,21 +57,21 @@ def wait_until_certificate_installed():
 
 
 # 检查系统代理是否设置正确
-def wait_until_proxy_setting(proxy_address):
+def wait_until_proxy_setting(mitm_proxy_address):
     while True:
-        if is_proxy_setting({"http": proxy_address, "https": proxy_address}):
+        if is_proxy_setting({"http": mitm_proxy_address, "https": mitm_proxy_address}):
             break
         else:
             print_info_message("\n(press <enter> to continue)")
             input()
 
 
-def wait_until_env_configured(proxy_address=None):
+def wait_until_env_configured(mitm_proxy_address = None):
     # 检查证书是否安装
     wait_until_certificate_installed()
 
     # 检查系统代理是否设置正确
-    wait_until_proxy_setting(proxy_address)
+    wait_until_proxy_setting(mitm_proxy_address)
 
 
 def print_logo():
