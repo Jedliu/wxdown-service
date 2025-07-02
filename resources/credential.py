7497b15d49629940e117e8b13b48c26026804a1a
jock <findsource@proton.me>
2025-07-02 11:39:49 +0800

    支持ipv6

--- resources/credential.py
+++ resources/credential.py
@@ -20,22 +20,19 @@ class ExtractWxCredentials:
 
     def response(self, flow: mitmproxy.http.HTTPFlow):
         # 检查请求的 URL 是否符合过滤器
-        if flow.request.url.startswith("https://mp.weixin.qq.com/s?__biz="):
-            print(f"命中请求")
-
+        parsed_url = urlparse(flow.request.url)
+        if parsed_url.path.startswith("/s?__biz="):
             # 提取 __biz 参数
-            parsed_url = urlparse(flow.request.url)
             query_params = parse_qs(parsed_url.query)
             biz = query_params.get('__biz', [None])[0]
             if biz:
                 # 提取响应头中的 Set-Cookie 数据
                 set_cookie_header = flow.response.headers.get("Set-Cookie")
                 if set_cookie_header:
-                    timestamp = int(time.time() * 1000)
                     self.cookies[biz] = {
                         "url": flow.request.url,
                         "set_cookie": set_cookie_header,
-                        "timestamp": timestamp,
+                        "timestamp": int(time.time() * 1000),
                     }
                     # 将 cookies 数据保存到文件中
                     with open(mitmproxy.ctx.options.credentials, "w") as file:
