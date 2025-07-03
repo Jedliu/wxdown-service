8c50f71dbedd237ef501e210cf511f0115a21d61
jock <findsource@proton.me>
2025-07-03 11:50:13 +0800

    优化代理检测

--- utils.py
+++ utils.py
@@ -4,30 +4,20 @@ import re
 import urllib.request
 from pathlib import Path
 
-import requests
-
 import version
+from logger import logger
 
 SRC_PATH = Path.absolute(Path(__file__)).parent
 LOGO_FILE = str(SRC_PATH / 'resources' / 'logo.txt')
 
+
 # 检查系统代理是否设置正确
 def check_system_proxy(mitm_proxy_address):
     proxy_obj = urllib.request.getproxies()
-
     details = f'将系统代理设置为 [bold green]{mitm_proxy_address.removeprefix('http://')}[/]\n当前系统代理为:\n{proxy_obj}'
 
-    try:
-        response = requests.get('http://mitm.it', proxies=proxy_obj, timeout=3).text
-    except Exception as e:
-        return False, '代理配置有误，请检查设置', details
-
-    traffic_not_passing = re.search(r'If you can see this, traffic is not passing through mitmproxy', response)
-    if traffic_not_passing:
-        # 流量未经过 mitmproxy
-        success = False
-    elif proxy_obj.keys() < {'http', 'https'}:
-        # 代理需要全部设置
+    if proxy_obj.keys() < {'http', 'https'}:
+        # http/https代理需要全部设置
         success = False
     elif proxy_obj['http'] != mitm_proxy_address or proxy_obj['https'] != mitm_proxy_address:
         success = False
@@ -36,6 +26,22 @@ def check_system_proxy(mitm_proxy_address):
 
     if not success:
         return False, '检测到系统的网络代理设置不正确', details
+
+
+    # 检测流量是否经过 mitmproxy
+    try:
+        with urllib.request.urlopen('http://mitm.it', timeout=10) as response:
+            content = response.read().decode('utf-8')
+            traffic_not_passing = re.search(r'If you can see this, traffic is not passing through mitmproxy', content)
+            if traffic_not_passing:
+                return False, '流量未经过 mitmproxy', ''
+    except TimeoutError as e:
+        logger.warning(f'检查http://mitm.it时超时: {e}')
+        return False, '检查代理超时，请稍后重试', '5秒后会自动重试'
+    except Exception as e:
+        logger.error(f'检查http://mitm.it时异常: {e}')
+        return False, '检查代理失败', '请联系开发者，并提供日志文件'
+
 
     return True, '成功', proxy_obj
 
