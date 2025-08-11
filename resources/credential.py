f2a74493bdaf4136ad895c6fd5fbe67d977660be
jock <findsource@proton.me>
2025-08-11 15:43:17 +0800

    credentials中增加name和avatar字段的解析

--- resources/credential.py
+++ resources/credential.py
@@ -4,7 +4,7 @@ import json
 from urllib.parse import urlparse, parse_qs
 import time
 from typing import Optional
-
+from bs4 import BeautifulSoup
 
 class ExtractWxCredentials:
     def __init__(self):
@@ -30,15 +30,35 @@ class ExtractWxCredentials:
             if biz:
                 # 提取响应头中的 Set-Cookie 数据
                 set_cookie_header = flow.response.headers.get("Set-Cookie")
+
+                # 提取 HTML 中的信息
+                name = None
+                avatar = None
+                if flow.response.content:
+                    try:
+                        soup = BeautifulSoup(flow.response.content, 'html.parser')
+                        name_tag = soup.css.select_one('.wx_follow_nickname')
+                        avatar_tag = soup.css.select_one('.wx_follow_avatar > img.wx_follow_avatar_pic')
+                        if name_tag:
+                            name = name_tag.get_text(strip=True)
+                        if avatar_tag:
+                            avatar = avatar_tag['src']
+                    except Exception as e:
+                        print(f"Error parsing HTML: {e}")
+
                 if set_cookie_header:
                     self.cookies[biz] = {
+                        "biz": biz,
+                        "name": name,
+                        "avatar": avatar,
                         "url": flow.request.url,
                         "set_cookie": set_cookie_header,
                         "timestamp": int(time.time() * 1000),
                     }
                     # 将 cookies 数据保存到文件中
-                    with open(mitmproxy.ctx.options.credentials, "w") as file:
-                        json.dump(list(self.cookies.values()), file, indent=4)
+                    if mitmproxy.ctx.options.credentials:
+                        with open(mitmproxy.ctx.options.credentials, "w", encoding="utf-8") as file:
+                            json.dump(list(self.cookies.values()), file, indent=4, ensure_ascii=False)
 
 addons = [
     ExtractWxCredentials(),
